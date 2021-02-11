9dad422918c76f1f8f5be1569abbc15cdd751005
gurus <gurus@192.168.2.13>
2021-02-11 15:58:28 -0500

    Committed the following changes 1.add calMax()method.2)add exception etc

--- src/tempaverage/TempAverage.java
+++ src/tempaverage/TempAverage.java
@@ -5,33 +5,46 @@
  */
 package tempaverage;
 import java.util.Scanner;
-/**
- *
- * @author gurus
- */
+
+
 public class TempAverage {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int temp[]=new int[14];
+        
+        try{
+        //declaring temp array of length 14
+        double temp[]=new double[14];
         Scanner input=new Scanner(System.in);
+        
+        //declaring object of class TempAverage
         TempAverage tem=new TempAverage();
         
         System.out.println("Enter the tempratures of last 14 days");
         for(int i=0;i<temp.length;i++){
-            System.out.print("Enter the value of tem["+i+"] = ");
-            temp[i]=input.nextInt();
+            System.out.print("Enter the value of temp["+i+"] = ");
+            temp[i]=input.nextDouble();
             System.out.println();
         }
-        System.out.println(tem.calcAvg(temp));
+        System.out.printf("The average of tempratures is %.2f F",tem.calcAvg(temp));
+        System.out.println();
         tem.gitHub();
-        
+        System.out.println("Now I am in NetBeans");
+        System.out.printf("Max temprature is %.2f F",tem.calcMax(temp));
+        System.out.println();
     }
-    public int calcAvg(int[] temp){
+        catch(Exception e){
+            System.out.println("Please enter onle intger or double values only");
+        }
+
+    }
+       
+    //function to calculate average of temprature which are not greter than and not less than -50
+    public double calcAvg(double[] temp){
         
-        int sum=0; 
+        double sum=0; 
         int count=0;
         for(int i=0;i<temp.length;i++){
             if(temp[i]<50 && temp[i]>-50){
@@ -42,10 +55,28 @@ public class TempAverage {
                 continue;
             }
         }
-        System.out.println(sum);
-        return  sum/count;
+        //System.out.println(sum);
+        return  sum/(double)count;
     }
+    
+    //This function is updated from remote repository
     public void gitHub(){
         System.out.println("Hello! I am in GitHub now ");   
     }
+    
+    //calculate max temprature 
+    public double calcMax(double [] temp){
+       double clone=0;
+       for(int i=0;i<temp.length-1;i++){
+           for(int j=0;j<temp.length-1;j++){
+                if(temp[j]>temp[j+1]){
+                    clone=temp[j];
+                    temp[j]=temp[j+1];
+                    temp[j+1]=clone;
+                }
+           }
+       }
+       //System.out.println("The Max temprature is = "+temp[temp.length-1]);
+       return temp[temp.length-1];
+   }
 }
